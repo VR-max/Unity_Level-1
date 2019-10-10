eba70540805bc146773ad5b2151eba4c2a9ce468
VR-max <vr@kvantorium.ru>
2019-10-10 20:20:59 +0300

    Третье практическое задание, перезалив

---
+++ FirstGame_2D_2/Assets/Scripts/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private int _hp;
+    
+
+    public void TakeDamage(int damage)
+    {
+        Debug.Log("Auch " + damage);
+            Destroy(gameObject);
+    }
+}
